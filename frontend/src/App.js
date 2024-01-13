a9352444d7f8c8e1f46d5a6f376d0a67b8fe1f89
hasharanethmi2020@gmail.com <hasharanethmi2020@gmail.com>
2024-01-13 21:39:50 +0530

    Add new pages to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import My_Work from './Pages/My-Work';
+import Recent_Workplaces from './Pages/Recent-Workplaces';
+
 
 function App() {
   return (
     <div>
-      App
+      {/* <Recent_Workplaces />  */}
+      <My_Work /> 
     </div>
   );
 }
