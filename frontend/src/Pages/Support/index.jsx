b7cd233bcbc12e41752ad9aef7def8de11220925
kgydilmith <dilmithyesitha@gmail.com>
2024-04-09 22:30:17 +0530

    new support and contact

--- frontend/src/Pages/Support/index.jsx
+++ frontend/src/Pages/Support/index.jsx
@@ -1,57 +1,66 @@
-import React from 'react';
-import Navbar from '../../Components/NavBar';
-import './index.css';
-import image1 from '../../Assets/q.jpg';
-import image2 from '../../Assets/x.jpg';
-import image3 from '../../Assets/y.jpg';
-import image4 from '../../Assets/z.jpg';
+import React from "react";
+import Navbar from "../../Components/NavBar";
+import Footer from "../../Components/Footer";
+import "./index.css";
+import image1 from "../../Assets/q.jpg";
+import image2 from "../../Assets/x.jpg";
+import image3 from "../../Assets/y.jpg";
+import image4 from "../../Assets/z.jpg";
 const Support = () => {
-
   const iconStyle = {
-    width: '60%', // Adjust the width as needed
-    height: 'auto',
-    margin: '30px',
-    border: '1px solid #ccc', // Add border style
-    borderRadius: '5px',
-    backgroundColor: '#f2f2f2', // Add background color
+    width: "60%", // Adjust the width as needed
+    height: "auto",
+    margin: "30px",
+    border: "1px solid #ccc", // Add border style
+    borderRadius: "5px",
+    backgroundColor: "#f2f2f2", // Add background color
   };
-  
 
   return (
     <div>
       <Navbar />
-      <div className="container">
-        
+      <div className="s-container">
         <ol>
-        
-        <ul>
-              <li>
-                Server up your frontend in port 4000
-              </li>
-              <li>
-                You can create your own workspace once you log in 
-                <img src={image1} alt="User-Created Collections Icon" style={iconStyle} />
-              </li>
-              <li>
-                In your workspace, create collection 
-                <img src={image2} alt="User-Created Collections Icon" style={iconStyle} />
-              </li>
-              <li>
-                Put request and response how do you like it to have
-                <img src={image3} alt="User-Created Collections Icon" style={iconStyle} />
-              </li>
-              <li>
-                Responses will go to your frontend and frontend will work 
-                <img src={image4} alt="User-Created Collections Icon" style={iconStyle} />
-              </li>
-            </ul>
-  
-
+          <ul>
+            <li>Server up your frontend in port 4000</li>
+            <li>
+              You can create your own workspace once you log in
+              <img
+                src={image1}
+                alt="User-Created Collections Icon"
+                style={iconStyle}
+              />
+            </li>
+            <li>
+              In your workspace, create collection
+              <img
+                src={image2}
+                alt="User-Created Collections Icon"
+                style={iconStyle}
+              />
+            </li>
+            <li>
+              Put request and response how do you like it to have
+              <img
+                src={image3}
+                alt="User-Created Collections Icon"
+                style={iconStyle}
+              />
+            </li>
+            <li>
+              Responses will go to your frontend and frontend will work
+              <img
+                src={image4}
+                alt="User-Created Collections Icon"
+                style={iconStyle}
+              />
+            </li>
+          </ul>
         </ol>
       </div>
-
+      <Footer />
     </div>
   );
-}
+};
 
 export default Support;
